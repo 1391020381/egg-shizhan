516690f18952381aabfcda7f28081890053e3c78
zhoujin <zhoujin@iboxpay.com>
2018-10-18 14:45:55 +0800

    postcomments

---
+++ app/migration/20181018063409-init-postComment.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    const S = Sequelize;
+    queryInterface.createTable('PostComments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: S.INTEGER,
+      },
+      post_id: S.INTEGER,
+      content: S.TEXT,
+      parent_id: S.INTEGER,
+      user_id: S.INTEGER,
+      created_at: {
+        allowNull: false,
+        type: S.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: S.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.dropTable('PostComments');
+  },
+};
