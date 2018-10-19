45a9944e59daf26d49e53575f5373fcbb20aac70
zhoujin <zhoujin@iboxpay.com>
2018-10-19 14:12:21 +0800

    ImageComment

---
+++ app/model/ImageComment.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = app => {
+  const { STRING, INTEGER } = app.Sequelize;
+  const ImageComment = app.model.define('ImageComment', {
+    id: INTEGER,
+    image_id: INTEGER,
+    content: STRING(255),
+    parent_id: INTEGER,
+    user_id: INTEGER,
+  });
+  ImageComment.associate = function() {
+    this.belongsTo(app.model.Image);
+    app.model.Image.hasMany(this);
+
+    this.belongsTo(app.model.User);
+    app.model.User.hasMany(this);
+    this.hasMany(this, {
+      foreignKey: 'parent_id',
+      as: 'childeren',
+    });
+    this.belongsTo(this, {
+      as: 'parent',
+    });
+  };
+  return ImageComment;
+}
+;
