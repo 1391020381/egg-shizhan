7a5d08e56dc133a5c13c5d06dfca6d5941b7a078
zhoujin <zhoujin@iboxpay.com>
2018-10-25 18:03:23 +0800

    update

---
+++ app/api.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = ctl => ({
+  post: {
+    '/signup': ctl.user.signUp, // 注册
+    '/signin': ctl.user.signIn,
+  },
+});
---
+++ app/util.js
@@ -0,0 +1,10 @@
+'use strict';
+const chalk = require('chalk');
+const { forEachObjIndexed } = require('ramda');
+function initRouterMap(prefix, maps, router) {
+  forEachObjIndexed((map, method) => {
+    forEachObjIndexed((controller, url) => {
+
+    }, map);
+  }, maps);
+}
